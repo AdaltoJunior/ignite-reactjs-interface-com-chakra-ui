d670a37511ada6bdd920ca8422a2dd0ee8cbda71
Adalto Junior <mendesrochajunior@gmail.com>
2022-02-16 14:58:57 -0300

    add background color to theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,8 +1,18 @@
 import { extendTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
+  colors: {
+    background: "#f5f8fa",
+  },
   fonts: {
     heading: 'Poppins, sans-serif',
     body: 'Poppins, sans-serif',
+  },
+  styles: {
+    global: {
+      body: {
+        background: 'background',
+      },
+    },
   },
 });
